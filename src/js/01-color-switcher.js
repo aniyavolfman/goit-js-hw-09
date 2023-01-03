42b8aa49058560c247d08df727607e1e7d434768
Anna Volfman <aniya_07@ukr.net>
2023-01-03 19:18:21 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+buttonStartEl.addEventListener('click', onButtonStartClick);
+buttonStopEl.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick() {
+    timerId = setInterval(() => {
+        document.body.setAttribute('style', `background-color:${getRandomHexColor()}`)
+    }, 1000);
+    buttonStartEl.disabled = true;
+    buttonStopEl.disabled = false;
+}
+
+function onButtonStopClick() {
+    clearInterval(timerId);
+    buttonStartEl.disabled = false;
+    buttonStopEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
